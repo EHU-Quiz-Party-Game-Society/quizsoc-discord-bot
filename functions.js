9dc845b83d62eb26e6b13317c60730f0f357968f
Penguin <penguingeek1@gmail.com>
2023-07-01 19:17:45 +0100

    Add new styling and re-request button

--- functions.js
+++ functions.js
@@ -9,7 +9,11 @@ export async function fetchRandomQuestion(interaction) {
         let data = await response.json();
         data = JSON.stringify(data);
         data = JSON.parse(data);
-        return data;
+        if(data.options.split(/\r\n|\r|\n/).length > 4) {
+            await fetchRandomQuestion(interaction)
+        } else {
+            return data;
+        }
     } catch(e) {
         console.error("Unable to reach API")
         Sentry.captureException(e, {
@@ -67,25 +71,26 @@ export async function getStatistics(interaction) {
     }
 }
 
-export function createButtonRow(interaction, question_id) {
+export function createButtonRow(interaction, data) {
     try {
+        let options = data.options.split(/\r\n|\r|\n/);
         return new MessageActionRow()
             .addComponents(
                 new MessageButton()
-                    .setCustomId(question_id + 'A')
-                    .setLabel('A')
+                    .setCustomId(data.id + 'A')
+                    .setLabel(options[0])
                     .setStyle('PRIMARY'),
                 new MessageButton()
-                    .setCustomId(question_id + 'B')
-                    .setLabel('B')
+                    .setCustomId(data.id + 'B')
+                    .setLabel(options[1])
                     .setStyle('PRIMARY'),
                 new MessageButton()
-                    .setCustomId(question_id + 'C')
-                    .setLabel('C')
+                    .setCustomId(data.id + 'C')
+                    .setLabel(options[2])
                     .setStyle('PRIMARY'),
                 new MessageButton()
-                    .setCustomId(question_id + 'D')
-                    .setLabel('D')
+                    .setCustomId(data.id + 'D')
+                    .setLabel(options[3])
                     .setStyle('PRIMARY'),
             );
     } catch(e) {
@@ -100,128 +105,148 @@ export function createButtonRow(interaction, question_id) {
     }
 }
 
-export function replyWithFeedback(interaction, question_id, correct) {
+export function replyWithFeedback(interaction, data, correct) {
     try {
-        switch(correct) {
-            case 'A':
+        //console.log(interaction.message.embeds[0])
+        let question_id = data.id;
+        let customId = interaction.customId.replace(/[0-9]/g,'');
+        let options = data.options.split(/\r\n|\r|\n/);
+        switch(true) {
+            case customId && correct === 'A':
                 return new MessageActionRow()
                     .addComponents(
-                        new MessageButton()
-                            .setCustomId(question_id + 'A')
-                            .setLabel('A')
-                            .setStyle('SUCCESS')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'B')
-                            .setLabel('B')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'C')
-                            .setLabel('C')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'D')
-                            .setLabel('D')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
+                        addOption(question_id + 'A', options[0], 'SUCCESS'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
                     );
-            case 'B':
+            case customId === 'A' && correct === 'B':
                 return new MessageActionRow()
                     .addComponents(
-                        new MessageButton()
-                            .setCustomId(question_id + 'A')
-                            .setLabel('A')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'B')
-                            .setLabel('B')
-                            .setStyle('SUCCESS')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'C')
-                            .setLabel('C')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'D')
-                            .setLabel('D')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
+                        addOption(question_id + 'A', options[0], 'DANGER'),
+                        addOption(question_id + 'B', options[1], 'SUCCESS'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
                     );
-            case 'C':
+            case customId === 'A' && correct === 'C':
                 return new MessageActionRow()
                     .addComponents(
-                        new MessageButton()
-                            .setCustomId(question_id + 'A')
-                            .setLabel('A')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'B')
-                            .setLabel('B')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'C')
-                            .setLabel('C')
-                            .setStyle('SUCCESS')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'D')
-                            .setLabel('D')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
+                        addOption(question_id + 'A', options[0], 'DANGER'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SUCCESS'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
                     );
-            case 'D':
+            case customId === 'A' && correct === 'D':
                 return new MessageActionRow()
                     .addComponents(
-                        new MessageButton()
-                            .setCustomId(question_id + 'A')
-                            .setLabel('A')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'B')
-                            .setLabel('B')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'C')
-                            .setLabel('C')
-                            .setStyle('DANGER')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'D')
-                            .setLabel('D')
-                            .setStyle('SUCCESS')
-                            .setDisabled(true),
+                        addOption(question_id + 'A', options[0], 'DANGER'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SUCCESS'),
+                    );
+            case customId === 'B' && correct === 'A':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SUCCESS'),
+                        addOption(question_id + 'B', options[1], 'DANGER'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId && correct === 'B':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SUCCESS'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId === 'B' && correct === 'C':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'DANGER'),
+                        addOption(question_id + 'C', options[2], 'SUCCESS'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId === 'B' && correct === 'D':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'DANGER'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SUCCESS'),
+                    );
+            case customId === 'C' && correct === 'A':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SUCCESS'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'DANGER'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId === 'C' && correct === 'B':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SUCCESS'),
+                        addOption(question_id + 'C', options[2], 'DANGER'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId && correct === 'C':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SUCCESS'),
+                        addOption(question_id + 'D', options[3], 'SECONDARY'),
+                    );
+            case customId === 'C' && correct === 'D':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'DANGER'),
+                        addOption(question_id + 'D', options[3], 'SUCCESS'),
+                    );
+            case customId === 'D' && correct === 'A':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SUCCESS'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'DAANGER'),
+                    );
+            case customId === 'D' && correct === 'B':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SUCCESS'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'DANGER'),
+                    );
+            case customId === 'D' && correct === 'C':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SUCCESS'),
+                        addOption(question_id + 'D', options[3], 'DANGER'),
+                    );
+            case customId && correct === 'D':
+                return new MessageActionRow()
+                    .addComponents(
+                        addOption(question_id + 'A', options[0], 'SECONDARY'),
+                        addOption(question_id + 'B', options[1], 'SECONDARY'),
+                        addOption(question_id + 'C', options[2], 'SECONDARY'),
+                        addOption(question_id + 'D', options[3], 'SUCCESS'),
                     );
             default:
                 return new MessageActionRow()
                     .addComponents(
-                        new MessageButton()
-                            .setCustomId(question_id + 'A')
-                            .setLabel('A')
-                            .setStyle('SECONDARY')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'B')
-                            .setLabel('B')
-                            .setStyle('SECONDARY')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'C')
-                            .setLabel('C')
-                            .setStyle('SECONDARY')
-                            .setDisabled(true),
-                        new MessageButton()
-                            .setCustomId(question_id + 'D')
-                            .setLabel('D')
-                            .setStyle('SECONDARY')
-                            .setDisabled(true),
+                        addOption(question_id + 'A', 'A', 'SECONDARY'),
+                        addOption(question_id + 'B', 'B', 'SECONDARY'),
+                        addOption(question_id + 'C', 'C', 'SECONDARY'),
+                        addOption(question_id + 'D', 'D', 'SECONDARY'),
                     );
         }
 
@@ -235,4 +260,23 @@ export function replyWithFeedback(interaction, question_id, correct) {
             level: 'error'
         })
     }
+}
+
+function addOption(question_id, option, type) {
+    return new MessageButton()
+        .setCustomId(question_id)
+        .setLabel(option)
+        .setStyle(type)
+        .setDisabled(true)
+}
+
+export function addCallToActionTryAgain() {
+    return new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('requestNewQuestion')
+                .setEmoji('🔄')
+                .setLabel('Get another question!')
+                .setStyle('PRIMARY')
+        )
 }
